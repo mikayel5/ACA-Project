db8bf9c80f11325573471a9755180624483f81a1
Vahan132 <vahanm21@gmail.com>
2019-03-24 15:18:34 +0400

    qq

--- src/HomePage/SearchArtistBio.js
+++ src/HomePage/SearchArtistBio.js
@@ -1,53 +1,27 @@
 import React, { Component } from 'react'
 
-
-
-
-export default class SearchArtistBio extends Component {
-    constructor (){
-        super ();
+class SearchArtistBio extends Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            inputValue: "",
-            isLoaded: false,
+            artistItem_bio: this.props.artistbio,
+            artistItem_img: this.props.artistimg,
         }
     }
+
     
-changing = event => this.setState({inputValue: event.target.value});
-
-SearchArtistBio (){
-
-     let url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${this.state.inputValue}&api_key=49edeb8bf7e07fe071335277a648f207&format=json`;
-    fetch(`url`)
-        .then((res) => res.json ())
-        .then((data) =>{
-            this.setState({
-                isLoaded: true,
-                artistItem_bio: data.artist.bio.content,
-                artistItem_img: data.artist.image[3]["#text"],
-            }
-            );    
-            console.log(data);
-            if (this.state.isLoaded) {
-                console.log(this.state.artistItem_img);
-            }
-            
-        })
-        .catch((error) => {
-            console.error(error)
-        });
-
+    render() {
+        return (
+        <div>
+            <div className = "artist_img">
+                <img src={this.state.artistItem_img} />
+            </div>
+            <div className ="atist_bio">
+                <p>{this.state.artistItem_bio}</p>
+            </div>
+        </div>
+        )
+    }
 }
 
-  render() {
-    return (
-      <div>
-        <div className = "artist_img">
-            <image></image>
-        </div>
-        <div className ="atist_bio">
-            <p>{}</p>
-        </div>
-      </div>
-    )
-  }
-}
+export default SearchArtistBio;
--- src/HomePage/SearchByArtist.js
+++ src/HomePage/SearchByArtist.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import SubContent from './SubContent';
 import Block from '../Block';
+import SearchArtistBio from './SearchArtistBio';
 
 
 class SearchByArtist extends Component {
@@ -10,6 +11,10 @@ class SearchByArtist extends Component {
             placeholder: this.props.placeholdervalue,
             inputvalue: "",
             videoId: [],
+            artistItem_bio: "",
+            artistItem_img: null,
+            isLoaded: false,
+
         }
     }
 
@@ -18,12 +23,26 @@ class SearchByArtist extends Component {
     }
 
     searchByArtist = () => {
-        console.log(this.state)
         const videoId = [];
-        this.setState({ videoId: [] })
+        this.setState({ videoId: [], isLoaded: false, artistItem_bio: "", artistItem_img: null })
         fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${this.state.inputvalue}&api_key=49edeb8bf7e07fe071335277a648f207&format=json`)
         .then(res => res.json())
         .then(myJson => myJson.results.artistmatches.artist[0].name)
+        .then(function(name) {
+            fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${name}&api_key=49edeb8bf7e07fe071335277a648f207&format=json`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    isLoaded: true,
+                    artistItem_bio: data.artist.bio.content,
+                    artistItem_img: data.artist.image[3]["#text"],
+                
+                })
+            }    
+            )
+
+            return name;
+        }.bind(this))
         .then(function(name) {
             this.setState({iframeTitle: this.name})
             fetch(`https://www.googleapis.com/youtube/v3/search?part=id&maxResults=5&order=relevance&q=${name}&key=AIzaSyDP7ztlVJ8pjrlFUaCsBMBtbjghLogw2fg`)
@@ -48,6 +67,9 @@ class SearchByArtist extends Component {
         return (
             <>
                 <SubContent placeholder={this.state.placeholder} onclick={this.searchByArtist} onchange={this.getInputValue} value={this.state.inputvalue}/>
+                {
+                    this.state.isLoaded ? <SearchArtistBio artistbio={this.state.artistItem_bio} artistimg={this.state.artistItem_img}/> : null
+                }
                 <div className='video_container'>
                     {
                         this.state.videoId.map((id, index) => <Block key={index} title={this.state.iframeTitle}url={`https://www.youtube.com/embed/${id}`}/>)
