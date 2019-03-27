5a74f887643dab9c5a10a8ddaf9098cde63c0627
Anghalatyan <a.anghalatyan@gmail.com>
2019-03-27 16:39:08 +0400

    add intro

---
+++ src/HomePage/Intro.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Intro extends Component {
+    render() {
+        return (
+            <div className="intro">
+                <p className="p-title">Browse your favorite song or artist with us</p>
+            </div>
+        )
+    }
+}
+
+export default Intro;
